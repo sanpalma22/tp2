d780d85e74e8e0639ef5b35a0c1e573bd3581ba6
sanpalma22 <spama0610@gmail.com>
2023-04-05 18:23:17 -0300

    Tp avanzando en parte 2

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<int, Persona> dicGente = new Dictionary<int, Persona>();
+        int menu, cont = 0, acumEdad = 0;
+        bool dniVerificado;
+        menu = ingresarMenu("Eliga una opcion");
+        while (menu > 0 && menu < 5)
+        {
+            switch (menu)
+            {
+                case 1:
+                    CargarPersona();
+                    break;
+                case 2:
+                    obtenerEstadisticas(dicGente);
+                    break;
+
+            }
+            menu = ingresarMenu("Eliga una opcion");
+        }
+
+
+
+        int ingresarMenu(string mensaje)
+        {
+            int menu;
+            Console.WriteLine(mensaje);
+            Console.WriteLine("1- Cargar Nueva Persona");
+            Console.WriteLine("2- Obtener Estadísticas del Censo");
+            Console.WriteLine("3- Buscar Persona");
+            Console.WriteLine("4- Modificar Mail de una Persona.");
+            Console.WriteLine("5- Salir");
+            menu = int.Parse(Console.ReadLine());
+            return menu;
+        }
+        void CargarPersona()
+        {
+
+            int dni;
+            do
+            {
+                dni = ingresarEntero("Ingrese el DNI");
+                dniVerificado = validarExistenciaDni(dni);
+            } while (!dniVerificado);
+            string ape = ingresarTexto("Ingrese su apellido").ToUpper();
+            string nom = ingresarTexto("Ingrese el nombre").ToUpper();
+            DateTime fNac = ingresarFecha("Ingrese su fecha de nacimiento. dd/mm/aaaa");
+            string email = ingresarTexto("Ingrese el su E-Mail").ToLower();
+            Persona unaPersona = new Persona(dni, ape, nom, fNac, email);
+                dicGente.Add(dni, unaPersona);
+            Console.WriteLine("Se ha creado la persona {0} y se ha agregado a la lista.", nom + " " + ape);
+        }
+        int ingresarEntero(string v)
+        {
+            int num;
+            Console.WriteLine(v);
+            num = int.Parse(Console.ReadLine());
+            return num;
+        }
+        string ingresarTexto(string v)
+        {
+            string text;
+            Console.WriteLine(v);
+            text = Console.ReadLine();
+            return text;
+        }
+        DateTime ingresarFecha(string v)
+        {
+            DateTime fecha;
+            string fechaString = ingresarTexto(v);
+            while (!DateTime.TryParse(fechaString, out fecha))
+            {
+                fechaString = ingresarTexto("Fecha inexistente, " + v);
+            }
+            return fecha;
+        }
+
+        bool validarExistenciaDni(int key)
+        {
+
+            bool keyPosible;
+            keyPosible = dicGente.ContainsKey(key) == false;
+            if (!keyPosible) Console.WriteLine("Ese DNI ya está ingresado");
+            return keyPosible;
+        }
+
+        void obtenerEstadisticas(Dictionary<int, Persona> dicGente)
+        {
+            Console.WriteLine("Estadisticas del censo:");
+            Console.WriteLine("Cantidad de personas: " + dicGente.Count);
+            Console.WriteLine("Cantidad de personas habilitadas para votar: "+ );
+
+            foreach(Persona){
+                if()
+            }
+        }
+    }
+}
---
+++ persona.cs
@@ -0,0 +1,33 @@
+using System;
+class Persona
+{
+    public int DNI { get; private set; }
+    public string Apellido { get; set; }
+    public string Nombre { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public string Email { get; set; }
+    public int Edad { get; set; }
+
+    public Persona()
+    {
+
+
+    }
+    public Persona(int dni = 0, string ape = "", string nom = "", DateTime fNac = new DateTime(), string email = "")
+    {
+        DNI = dni;
+        Apellido = ape;
+        Nombre = nom;
+        FechaNacimiento = fNac;
+        Email = email;
+    }
+
+
+    public int MiEdad()
+    {
+        DateTime FechaNacimientoHoy = new DateTime(DateTime.Today.Year, FechaNacimiento.Month, FechaNacimiento.Day);
+        if (FechaNacimientoHoy < DateTime.Today) Edad = DateTime.Today.Year - FechaNacimiento.Year;
+        else Edad = DateTime.Today.Year - FechaNacimiento.Year - 1;
+        return Edad;
+    }
+}
